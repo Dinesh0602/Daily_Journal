8667b46ad3b8bb43da9481c4b219ec29db95f178
Dinesh Buruboyina <buruboyinadinesh@gmail.com>
2023-05-10 21:09:56 +0530

    Final Commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _=require('lodash');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -14,10 +17,10 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect('mongodb://localhost:27017/blogDB')
+mongoose.connect('mongodb://localhost:27017/blogDB',{useNewUrlParser: true,useUnifiedTopology: true})
 
 const blogSchema={
   postTitle:String,
@@ -65,9 +68,31 @@ res.redirect('/');
 app.get('/posts/:postId',function(req, res){
 
   Blog.findById(req.params.postId,function(err, blog){
-    res.render('post',{title:blog.postTitle,body:blog.postBody});
+    res.render('post',{title:blog.postTitle,body:blog.postBody,postId:req.params.postId});
   })
 })
+
+app.put('/edit', function(req,res){
+     Blog.findOneAndUpdate({_id:req.body.postId},{postTitle:req.body.title,postBody:req.body.content},{overwrite:true},function(err,blog){
+      if(!err){
+        res.redirect('/');
+      }
+      else{
+        console.log(err);
+      
+      }
+    });
+    
+    })
+ 
+
+app.get('/post/edit/:postId',function(req, res){
+
+  Blog.findById(req.params.postId,function(err, blog){
+    res.render('edit',{title:blog.postTitle,body:blog.postBody,postId:req.params.postId});
+  })
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
